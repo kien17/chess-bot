1ae63f9b72a41308cfec62d974f420b0ddef7143
Trung Kien <kien36503@gmail.com>
2026-03-25 18:13:23 +0700

    update bot MCTS

---
+++ bot_MCTS/mcts_bot.py
@@ -0,0 +1,5 @@
+import chess
+from mcts_node import MCTS_node
+
+class MCTS_bot:
+    pass
---
+++ bot_MCTS/mcts_node.py
@@ -0,0 +1,10 @@
+import chess
+
+class MCTS_node:
+    def __init__(self, board: chess.Board, parent=None, move=None):
+        self.board = board
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.visits = 0
+        self.wins = 0
